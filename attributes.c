c79d0ed6dabe539f380354cc28f600d810ee451a
Samuel CORON <scoron@e2r9p4.42.fr>
2019-03-03 12:50:41 +0100

    quick push

--- attributes.c
+++ attributes.c
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   support.c                                          :+:      :+:    :+:   */
+/*   attributes.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: scoron <scoron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/03 12:40:13 by scoron            #+#    #+#             */
-/*   Updated: 2019/03/03 12:40:15 by scoron           ###   ########.fr       */
+/*   Created: 2019/03/03 12:42:04 by scoron            #+#    #+#             */
+/*   Updated: 2019/03/03 12:50:34 by scoron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_ls.h"
 
-void				free_lsbox(t_lsbox *lsbox)
-{
-	(void)lsbox;
-}
 
-void				ls_error(t_lsbox *lsbox, char *str)
+
+void				attributes(t_lsbox *lsbox)
 {
-	ft_putstr_fd(str, 2);
-	write(2, "\n", 1);
-	free_lsbox(lsbox);
-	exit(1);
+	t_arg_ch			*arg_ch;
+	struct stat			func;
+
+	arg_ch = lsbox->current_arg_ch;
+	while (arg_ch)
+	{
+		if (!lstat(arg_ch->data.path, &func))
+			read_f(lsbox, arg_ch, &func);
+		else
+			arg_ch->data.n_file = 1;
+		arg_ch = arg_ch->next;
+	}
 }
